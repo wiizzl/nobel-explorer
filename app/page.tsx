62f1909aa30caccbb412931801455722557af7a9
wiizzl <wiizz76.contact@gmail.com>
2025-03-29 13:09:14 +0100

    feat: enhance type safety for data fetching in HomePage by specifying Laureates and Prizes types

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { MaxWidthWrapper } from "@/components/layout/max-width-wrapper";
 
 import { fetchData } from "@/features/data/data.action";
+import { type Laureates } from "@/features/data/schemas/laureate.schema";
+import { type Prizes } from "@/features/data/schemas/prize.schema";
 import { SearchBar } from "@/features/search/SearchBar";
 
 export default async function HomePage() {
-  const [laureatesData, prizesData] = await Promise.all([fetchData("laureate"), fetchData("prize")]);
+  const [laureatesData, prizesData]: [Laureates, Prizes] = await Promise.all([
+    fetchData("laureate") as Promise<Laureates>,
+    fetchData("prize") as Promise<Prizes>,
+  ]);
 
   return (
     <main className="min-h-screen">
